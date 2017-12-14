c0c19542a08417b253161e30ea32671a22feee2b
obedgm <obedgm33@gmail.com>
2017-12-13 18:32:25 -0600

    First Commit

---
+++ email.php
@@ -0,0 +1,22 @@
+<?php
+$subject = $_REQUEST['subject'] . ' Mensaje: De la página web ' ; // Subject of your email
+$to = 'hola@meshika.mx';  //Recipient's E-mail
+
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= "From: " . $_REQUEST['email'] . "\r\n"; // Sender's E-mail
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+$message .= 'Name: ' . $_REQUEST['name'] . "<br>";
+$message .= $_REQUEST['message'];
+
+if (@mail($to, $subject, $message, $headers))
+{
+	// Transfer the value 'sent' to ajax function for showing success message.
+	echo 'sent';
+}
+else
+{
+	// Transfer the value 'failed' to ajax function for showing error message.
+	echo 'failed';
+}
+?>
